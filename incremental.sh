3e9c1d957e85c2d4e2fdb5438043d101a75fba76
Vinicius de Ávila Bezerra <85585157+Vini-boat@users.noreply.github.com>
2025-05-30 09:48:52 -0300

    Permite fazer o backup somente de bancos esprecíficos

--- incremental.sh
+++ incremental.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export CONF="./bancos.txt"
-export BACKUP_DIR="/backup/incremental/temp/"
-export LOG_DIR="/sky/scripts/incremental/logs/"
+export CONF="/sky/scripts/scripts_rotinas_hq/bancos.txt"
+export BACKUP_DIR="/backup/incremental/"
+export LOG_DIR="/sky/logs/incremental/"
 export FBUSER="SYSDBA"
-export FBPASS="skycl0ud"
+export FBPASS="5kyC10uD"
 export SERVER="localhost/3050"
 export LEVEL="$1"
 export KEEP_FILES="$2"
+export CONF_OVERRIDE="$3"
 
 logMsg () {
         echo -e "$(date): $1" >> $LOG_DIR'incremental.log'
@@ -16,17 +17,21 @@ logMsg () {
 
 
 [ ! -e $LOG_DIR ] && logMsg "Criando diretório $LOG_DIR" && mkdir -p $LOG_DIR
-[ ! "$#" -eq 2 ] && logMsg "é necessário especificar o nível do backup e quantidade de arquivos. eg.: incremental.sh 2 21" && exit 1
+[ "$#" -lt 2 ] && logMsg "é necessário especificar o nível do backup e quantidade de arquivos. eg.: incremental.sh 2 21" && exit 1
 [ $LEVEL -lt 0 ] && logMsg "o nível  deve ser 0, 1 ou 2" && exit 1
 [ $LEVEL -gt 2 ] && logMsg "o nível deve ser 0, 1 ou 2" && exit 1
 [ ! $KEEP_FILES -gt 0 ] && logMsg "a quantidade de arquivos a manter deve ser maior que 0" && exit 1
 which nbackup &> /dev/null
 [ $? -ne 0 ] && logMsg "Comando nbackup não existe" && exit 1
-[ ! -e $CONF ] && logMsg "Arquivo de configuração $CONF não existe" && exit 1
 [ ! -e $BACKUP_DIR ] && logMsg "Criando diretório $BACKUP_DIR" && mkdir -p $BACKUP_DIR
 
+if [ -n "$CONF_OVERRIDE" ]; then
+        bancos="$CONF_OVERRIDE"
+else
+        [ ! -e $CONF ] && logMsg "Arquivo de configuração $CONF não existe" && exit 1
+        bancos=$(grep '^[^#[:space:]]' "$CONF")
+fi
 
-bancos=$(grep '^[^#[:space:]]' "$CONF")
 for banco in $bancos; do
         cliente=$(echo $banco | cut -d "/" -f 2);
         nomebanco=$(echo ${banco%.?db} | cut -d "/" -f 4);
@@ -37,7 +42,7 @@ for banco in $bancos; do
         echo "$banco ->  $backup_name";
         nbackup -user $FBUSER -pass $FBPASS -b $LEVEL "$SERVER:$banco" "$backup_name";
         if [[ -e $backup_name ]]; then
-		ls -t $final_dir*.nb$LEVEL 2>/dev/null | tail -n +$(($KEEP_FILES + 1)) | xargs rm -f;
+                ls -t $final_dir*.nb$LEVEL 2>/dev/null | tail -n +$(($KEEP_FILES + 1)) | xargs rm -f;
         else
                 logMsg "Erro ao realizar o backup $backup_name de $banco"
         fi
